b3f8cabcf7f808303ec16a0b61fce9a261c1a7b4
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-03-12 08:28:18 -0700

    rearranged-root-directory-for-deployment-and-start-work-on-post

---
+++ src/components/Protected.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { getPhotos } from '../actions';
+
+import Photo from './Photo';
+
+class Protected extends React.Component {
+    componentDidMount() {
+        this.props.getPhotos();
+    }
+
+    render() {
+        return (
+          <div>
+            <h1>Expat Journey List</h1>
+            {this.props.gettingPhotos ? (
+              <h3>Getting Photos</h3>
+            ) : (
+              <div>
+                {this.props.all.map(all => {
+                  return (
+                  <div>  
+                    <Photo
+                        img_url={all.img_url}
+                        key={all.id}     
+                    />
+                  </div>
+                  );
+                })}
+              </div>
+            )}
+            {this.props.error !== "" ? <h4>{this.props.error}</h4> : null}            
+          </div>
+          
+        );
+      }
+    }
+    
+    const mapStateToProps = state => {
+      return {
+        all: state.all,
+        error: state.error,
+        gettingPhotos: state.gettingPhotos
+      };
+    };
+    
+    export default connect(mapStateToProps, { getPhotos })(Protected);
